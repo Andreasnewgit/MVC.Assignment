56f8dbb71ce5bc9c8ea4c88754c21fc6d99efc12
Andreas <andreas.lyckholm@gmail.com>
2022-11-10 13:25:18 +0100

    Week 2 Assignment 1, partially done

--- CodeAlongGr10/Controllers/PeopleController.cs
+++ CodeAlongGr10/Controllers/PeopleController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MCV.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MCV.Controllers
 {
     public class PeopleController : Controller
     {
-        public IActionResult PeopleIndex()
+        public IActionResult Index()
+        {
+            if (PersonViewModel.listOfPeople.Count == 0)
+                PersonViewModel.GeneratePeople();
+
+            PersonViewModel vm = new PersonViewModel();
+
+            vm.tempList = PersonViewModel.listOfPeople;
+            return View(vm);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Person person)
+        {           
+            if (person != null)
+            {
+                person.Id = Guid.NewGuid().ToString();
+                PersonViewModel.listOfPeople.Add(person);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(string id)
+        {
+            var personToDelete = PersonViewModel.listOfPeople.First(x => x.Id == id);
+            if(personToDelete != null)
+            {
+                PersonViewModel.listOfPeople.Remove(personToDelete);
+
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
---
+++ CodeAlongGr10/Models/PersonViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MCV.Models
+{
+    public class PersonViewModel
+    {
+        public static List<Person> listOfPeople = new List<Person>();
+
+        public List<Person> tempList = new List<Person>();
+
+        // public Person person { get; set; }
+
+        public static void GeneratePeople()
+        {
+            listOfPeople.Add(new Person { Id = Guid.NewGuid().ToString(), Name="Andreas Lyckholm", PhoneNumber="0738382888", City ="Gothenburg"});
+            listOfPeople.Add(new Person { Id = Guid.NewGuid().ToString(), Name = "Sven Svensson", PhoneNumber = "8739493999", City = "Göteborg" });
+            listOfPeople.Add(new Person { Id = Guid.NewGuid().ToString(), Name = "Sten Sture", PhoneNumber = "087172777", City = "Stockholm" });
+        }
+    }
+}
