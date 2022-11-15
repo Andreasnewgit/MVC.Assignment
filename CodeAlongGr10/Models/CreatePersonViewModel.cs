ae98ee6b207974c669b135cbb4a23fb8dc58eb86
AndreasLy <andreas.lyckholm@gmail.com>
2022-11-15 17:47:37 +0100

    Reset for week 2

---
+++ CodeAlongGr10/Models/CreatePersonViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MCV.Models
+{
+    public class CreatePersonViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+    }
+}
