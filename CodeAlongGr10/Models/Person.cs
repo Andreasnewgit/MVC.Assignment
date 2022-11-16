59adf40ac5c91c3b18fb1d765011135dd49254a2
Andreas <andreas.lyckholm@gmail.com>
2022-11-16 12:05:54 +0100

    Week2

---
+++ CodeAlongGr10/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace MCV.Models
+{
+    public class Person
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }    
+        public string City { get; set; }
+        // Can be null
+        public string? PhoneNumber { get; set; }
+    }
+}
---
+++ CodeAlongGr10/Models/PersonViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace MCV.Models
+{
+    public class PersonViewModel
+    {
+        public static List<Person> ListOfPeople = new List<Person>();
+
+        // Fills from the list above, tempList used for filtering(searching
+        public List<Person> tempList = new List<Person>();
+
+        public static void GeneratePeopleTest()
+        {
+            ListOfPeople.Add(new Person { Id = Guid.NewGuid().ToString(), Name="Andreas lyckholm", City="Göteborg", PhoneNumber="0768382888"});
+            ListOfPeople.Add(new Person { Id = Guid.NewGuid().ToString(), Name="Test McTest", City="Stockholm", PhoneNumber="08123456"}); 
+            ListOfPeople.Add(new Person { Id = Guid.NewGuid().ToString(), Name="Christoffer Lyckholm", City="Göteborg", PhoneNumber="0767771717" }); 
+            ListOfPeople.Add(new Person { Id = Guid.NewGuid().ToString(), Name="Håkan Skeneby", City="Mora", PhoneNumber="031183439" });
+        }
+    }
+}
